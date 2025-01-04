96a39a775e2dfeb5c531f4e527e6b4872afb53a1
heehunjung <heehunjung3658@gmail.com>
2025-01-04 23:34:59 +0900

    🎉  chore: 스프링 부트 세팅
    java - 17, spring boot - 3.4.1

---
+++ src/main/java/com/ssmoker/smoker/SmokerApplication.java
@@ -0,0 +1,13 @@
+package com.ssmoker.smoker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmokerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssmoker/smoker/SmokerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssmoker.smoker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmokerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
