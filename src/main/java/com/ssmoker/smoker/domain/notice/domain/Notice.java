6656f8c10f908251a072b3d64f7bde233583d048
heehunjung <heehunjung3658@gmail.com>
2025-01-05 17:46:20 +0900

    ✨  초기 엔티티 설계

---
+++ src/main/java/com/ssmoker/smoker/domain/notice/domain/Notice.java
@@ -0,0 +1,28 @@
+package com.ssmoker.smoker.domain.notice.domain;
+
+import com.ssmoker.smoker.global.common.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+public class Notice extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+    
+    @Column(length = 1000, nullable = false)
+    private String content;
+}
---
+++ src/main/java/com/ssmoker/smoker/domain/updatedHistory/domain/UpdatedHistory.java
@@ -0,0 +1,37 @@
+package com.ssmoker.smoker.domain.updatedHistory.domain;
+
+import com.ssmoker.smoker.domain.member.domain.Member;
+import com.ssmoker.smoker.domain.smokingArea.domain.SmokingArea;
+import com.ssmoker.smoker.global.common.BaseEntity;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+public class UpdatedHistory extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    //행위 ENUM 추가 ?
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
+    @JoinColumn(name = "smoking_area_id", nullable = false)
+    private SmokingArea smokingArea;
+}
