45793d83d9e6c3ef524b565dd5db895f8d252a5c
heehunjung <heehunjung3658@gmail.com>
2025-01-05 13:59:22 +0900

    ✨  BaseEntity 생성

---
+++ src/main/java/com/ssmoker/smoker/global/common/BaseEntity.java
@@ -0,0 +1,22 @@
+package com.ssmoker.smoker.global.common;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public class BaseEntity {
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
+
