e0ee56045e2ee89e4f16869f0bbd51ed8559a9b2
heehunjung <heehunjung3658@gmail.com>
2025-01-06 00:45:01 +0900

    ✏️  API 응답 통일

---
+++ src/main/java/com/ssmoker/smoker/global/apiPayload/code/BaseCode.java
@@ -0,0 +1,8 @@
+package com.ssmoker.smoker.global.apiPayload.code;
+
+public interface BaseCode {
+
+    ReasonDTO getReason();
+
+    ReasonDTO getReasonHttpStatus();
+}
---
+++ src/main/java/com/ssmoker/smoker/global/apiPayload/code/SuccessStatus.java
@@ -0,0 +1,37 @@
+package com.ssmoker.smoker.global.apiPayload.code;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum SuccessStatus implements BaseCode {
+
+    _OK(HttpStatus.OK, "COMMON", "성공입니다.");
+    //TODO: 다양한 성공 응답 추가...
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+
+    @Override
+    public ReasonDTO getReason() {
+        return ReasonDTO.builder()
+                .message(message)
+                .code(code)
+                .isSuccess(true)
+                .build();
+    }
+
+    @Override
+    public ReasonDTO getReasonHttpStatus() {
+        return ReasonDTO.builder()
+                .message(message)
+                .code(code)
+                .isSuccess(true)
+                .httpStatus(httpStatus)
+                .build()
+                ;
+    }
+}
