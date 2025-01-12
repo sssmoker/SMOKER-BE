6fece5633a237112aaf98379dd8ab12d23daf4ab
heehunjung <heehunjung3658@gmail.com>
2025-01-12 15:47:34 +0900

    ✨  흡연 구역 상세 조회 API 기본 구현

---
+++ src/main/java/com/ssmoker/smoker/domain/smokingArea/controller/SmokingAreaController.java
@@ -0,0 +1,29 @@
+package com.ssmoker.smoker.domain.smokingArea.controller;
+
+import com.ssmoker.smoker.domain.smokingArea.service.SmokingAreaService;
+import com.ssmoker.smoker.domain.smokingArea.service.dto.SmokingAreaInfoResponse;
+import com.ssmoker.smoker.global.apiPayload.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/smoking-area")
+public class SmokingAreaController {
+
+    private final SmokingAreaService smokingAreaService;
+
+    //Swagger 를 어디까지 적어야할 지
+    @Operation(summary = "흡연 구역 상세 조회")
+    @GetMapping("/{smokingAreaId}")
+    public ApiResponse<SmokingAreaInfoResponse> getSmokingArea(@PathVariable Long smokingAreaId) {
+        SmokingAreaInfoResponse result = smokingAreaService.getSmokingAreaInfo(smokingAreaId);
+
+        return ApiResponse.onSuccess(result);
+    }
+}
