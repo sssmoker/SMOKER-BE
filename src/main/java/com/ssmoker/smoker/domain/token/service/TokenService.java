62f4655b1a6f6dec231a391bc65041dd2e700299
Sojeong0430 <dlthwjd1629@gmail.com>
2025-01-19 14:50:42 +0900

    ✨ 회원탈퇴 구현

--- src/main/java/com/ssmoker/smoker/domain/token/service/TokenService.java
+++ src/main/java/com/ssmoker/smoker/domain/token/service/TokenService.java
@@ -2,6 +2,7 @@ package com.ssmoker.smoker.domain.token.service;
 
 import static com.ssmoker.smoker.global.exception.code.ErrorStatus.NOT_CONTAIN_TOKEN;
 
+import com.ssmoker.smoker.domain.member.repository.MemberRepository;
 import com.ssmoker.smoker.global.security.authDTO.AuthResponseDTO.TokenResponse;
 import com.ssmoker.smoker.domain.token.domain.Token;
 import com.ssmoker.smoker.domain.token.repository.TokenRepository;
@@ -21,6 +22,7 @@ public class TokenService {
 
     private final JwtTokenProvider jwtTokenProvider;
     private final TokenRepository tokenRepository;
+    private final MemberRepository memberRepository;
 
     public TokenResponse reissueToken(HttpServletRequest request) {
         String refreshToken = jwtTokenProvider.extractToken(request);
@@ -56,5 +58,12 @@ public class TokenService {
         jwtTokenProvider.isTokenValid(refreshToken);
         Long memberId = jwtTokenProvider.getId(refreshToken);
         return memberId;
+    }
+
+    public void deleteUser(HttpServletRequest request) {
+        String refreshToken = jwtTokenProvider.extractToken(request);
+        long memberId = validateRefreshToken(refreshToken);
+        memberRepository.deleteById(memberId);
+        deleteToken(request);
     }
 }
--- src/main/java/com/ssmoker/smoker/global/security/controller/AuthController.java
+++ src/main/java/com/ssmoker/smoker/global/security/controller/AuthController.java
@@ -1,10 +1,12 @@
 package com.ssmoker.smoker.global.security.controller;
 
+import com.ssmoker.smoker.domain.token.service.TokenService;
 import com.ssmoker.smoker.global.security.authDTO.AuthResponseDTO;
 import com.ssmoker.smoker.global.security.authDTO.AuthResponseDTO.OAuthResponse;
 import com.ssmoker.smoker.global.apiPayload.ApiResponse;
 import com.ssmoker.smoker.global.apiPayload.code.SuccessStatus;
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final AuthService authService;
+    private final TokenService tokenService;
 
     @ResponseStatus(code = HttpStatus.OK)
     @Operation(summary = "카카오 로그인 API", description = "카카오 로그인 및 회원 가입을 진행하는 API입니다. 인가코드를 넘겨주세요")
@@ -34,4 +37,11 @@ public class AuthController {
         return ApiResponse.of(SuccessStatus.USER_LOGIN_OK, authService.GoogleLogin(code));
     } //구글 로그인
 
+    @ResponseStatus(code = HttpStatus.OK)
+    @Operation(summary = "회원 탈퇴 API", description = "회원 탈퇴 API 입니다")
+    @GetMapping("/deactivate")
+    public ApiResponse<Void> AccountDeactivation(HttpServletRequest request) {
+        tokenService.deleteUser(request);
+        return ApiResponse.of(SuccessStatus.USER_DELETE_OK,null);
+    } //회원 탈퇴
 }
