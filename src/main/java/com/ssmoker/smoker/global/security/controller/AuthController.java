1b531c1b8d79cfa34a22d72324cca07e77c5b50f
Sojeong0430 <dlthwjd1629@gmail.com>
2025-01-19 14:55:43 +0900

    ♻️ 멤버 컨트롤러 수정

--- src/main/java/com/ssmoker/smoker/global/security/controller/AuthController.java
+++ src/main/java/com/ssmoker/smoker/global/security/controller/AuthController.java
@@ -9,11 +9,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -39,7 +35,7 @@ public class AuthController {
 
     @ResponseStatus(code = HttpStatus.OK)
     @Operation(summary = "회원 탈퇴 API", description = "회원 탈퇴 API 입니다")
-    @GetMapping("/deactivate")
+    @DeleteMapping("/deactivate")
     public ApiResponse<Void> AccountDeactivation(HttpServletRequest request) {
         tokenService.deleteUser(request);
         return ApiResponse.of(SuccessStatus.USER_DELETE_OK,null);
