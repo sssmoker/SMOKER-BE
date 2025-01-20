9176c16c33463f32deede62df0952be52ed944b5
heehunjung <heehunjung3658@gmail.com>
2025-01-20 21:31:12 +0900

    ♻️  리뷰 관련 코드 디렉토리 분리

--- src/main/java/com/ssmoker/smoker/domain/smokingArea/domain/SmokingArea.java
+++ src/main/java/com/ssmoker/smoker/domain/smokingArea/domain/SmokingArea.java
@@ -39,8 +39,6 @@ public class SmokingArea extends BaseEntity {
     private Feature feature;
 
     private String imageUrl;
-    /*@Column(nullable = false)
-    private Boolean isApproved;*/
 
     @OneToMany(mappedBy = "smokingArea", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Review> reviews = new ArrayList<>();
