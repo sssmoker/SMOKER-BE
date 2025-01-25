4438accfaa3037a2900a5f3fdedb89c4d7ffb2bf
Sojeong0430 <dlthwjd1629@gmail.com>
2025-01-26 02:01:45 +0900

    ♻️ 리뷰 최신 순 정렬로 수정

--- src/main/java/com/ssmoker/smoker/domain/member/converter/MemberConverter.java
+++ src/main/java/com/ssmoker/smoker/domain/member/converter/MemberConverter.java
@@ -4,6 +4,7 @@ import com.ssmoker.smoker.domain.member.dto.MemberResponseDTO;
 import com.ssmoker.smoker.domain.review.domain.Review;
 import org.springframework.data.domain.Page;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -24,7 +25,7 @@ public class MemberConverter {
     public static MemberResponseDTO.MemberReviewListDTO toMemberReviewListDTO(Page<Review> reviews) {
         List<MemberResponseDTO.MemberReviewDTO> memberReviewDTOList = reviews.stream()
                 .map(MemberConverter::toMemberReviewDTO).collect(Collectors.toList());
-
+        Collections.reverse(memberReviewDTOList);
         return MemberResponseDTO.MemberReviewListDTO.builder()
                 .MemberReviewList(memberReviewDTOList)
                 .totalPage(reviews.getTotalPages())
