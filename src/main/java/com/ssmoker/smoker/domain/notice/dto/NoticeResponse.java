c0c1de3db0051c04acda5346421d670ab91e7572
seoyeon0103 <angel6462@naver.com>
2025-01-28 12:02:07 +0900

    ✨ successStatus에 관련 status 추가하기

---
+++ src/main/java/com/ssmoker/smoker/domain/notice/dto/NoticeResponse.java
@@ -0,0 +1,25 @@
+package com.ssmoker.smoker.domain.notice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+public class NoticeResponse {
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class NoticeListResponse{
+        List<>
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class NoticeViewResponse{
+
+    }
+}
