e115e6bb49a7a91fc2de1251c2ca30c701595b45
heehun <slrkqk2@naver.com>
2025-01-29 23:10:14 +0900

    ✨  health check용 api 설정

---
+++ src/main/java/com/ssmoker/smoker/global/aws/eb/HealthCheck.java
@@ -0,0 +1,13 @@
+package com.ssmoker.smoker.global.aws.eb;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheck {
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "Health Check";
+    }
+}
