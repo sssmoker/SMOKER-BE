9cbb70e7d866b9b7541f2906dccc5895d1c0a0fb
20210699 <20210699@duksung.ac.kr>
2025-01-31 05:35:27 +0900

    ✨ Feat: 흡연 구역 이미지 S3 등록 기능 개발
    keyName 추후 수정

--- src/main/java/com/ssmoker/smoker/domain/smokingArea/service/GoogleVisionOCRService.java
+++ src/main/java/com/ssmoker/smoker/domain/smokingArea/service/GoogleVisionOCRService.java
@@ -2,9 +2,10 @@ package com.ssmoker.smoker.domain.smokingArea.service;
 
 import com.google.cloud.vision.v1.*;
 import com.google.protobuf.ByteString;
-import com.ssmoker.smoker.domain.smokingArea.exception.SmokingAreaNotFoundException;
+import com.ssmoker.smoker.global.aws.s3.AmazonS3Manager;
 import com.ssmoker.smoker.global.exception.GeneralException;
 import com.ssmoker.smoker.global.exception.code.ErrorStatus;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -12,10 +13,17 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class GoogleVisionOCRService {
+
+    private final AmazonS3Manager amazonS3Manager;
+
     // 이미지 내 text 검출
     public String detectText(MultipartFile file) throws IOException {
         List<AnnotateImageRequest> requests = new ArrayList<>();
@@ -39,8 +47,27 @@ public class GoogleVisionOCRService {
 
     // 검출된 text 검증
     public boolean isSmokingArea(String text) {
-        if (text.contains("흡연") || text.toLowerCase().contains("smoking area")) return true;
+        if (text == null || text.isEmpty()) {
+            return false;
+        }
 
-        throw new SmokingAreaNotFoundException(ErrorStatus.SMOKING_KEYWORD_NOT_FOUND);
+        String lowerText = text.toLowerCase();
+
+        Pattern noSmokingPattern = Pattern.compile("\\bno\\s+smoking\\b|\\bno\\s+smoking\\s+zone\\b", Pattern.CASE_INSENSITIVE);
+        Matcher noSmokingMatcher = noSmokingPattern.matcher(lowerText);
+        if (noSmokingMatcher.find()) return false;
+
+        Pattern smokingPattern = Pattern.compile("\\b흡연\\b|\\bsmoking\\s+area\\b|\\bsmoking\\s+zone\\b", Pattern.CASE_INSENSITIVE);
+        Matcher smokingMatcher = smokingPattern.matcher(lowerText);
+        return smokingMatcher.find();
+    }
+
+
+    // S3에 이미지 업로드
+    public String uploadSmokingAreaImage(MultipartFile file) {
+        final String uuid = UUID.randomUUID().toString();
+        final String keyName = amazonS3Manager.generateSmokingAreaKeyName("예시 이름", uuid);
+
+        return amazonS3Manager.uploadFile(keyName, file);
     }
 }
