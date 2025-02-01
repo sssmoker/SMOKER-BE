df4a853df0a8fbf812d7deaa5dd139a2d04fd73b
heehunjung <heehunjung3658@gmail.com>
2025-02-01 20:12:23 +0900

    ♻️  bulk insert 구현

--- src/main/java/com/ssmoker/smoker/domain/smokingArea/repository/SmokingAreaRepository.java
+++ src/main/java/com/ssmoker/smoker/domain/smokingArea/repository/SmokingAreaRepository.java
@@ -1,14 +1,12 @@
 package com.ssmoker.smoker.domain.smokingArea.repository;
 
 import com.ssmoker.smoker.domain.smokingArea.domain.SmokingArea;
-import com.ssmoker.smoker.domain.smokingArea.dto.SmokingAreaDetailResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 
 @Repository
