f4f74fd068e88d27c6168fceb8c38f3772fd383b
Sojeong0430 <dlthwjd1629@gmail.com>
2025-02-03 23:22:07 +0900

    ✨Question Entity 추가

---
+++ src/main/java/com/ssmoker/smoker/domain/question/domain/Question.java
@@ -0,0 +1,22 @@
+package com.ssmoker.smoker.domain.question.domain;
+
+import com.ssmoker.smoker.global.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Question extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(length = 1000, nullable = false)
+    private String content;
+}
