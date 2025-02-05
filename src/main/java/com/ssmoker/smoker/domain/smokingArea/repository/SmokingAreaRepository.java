6b08416ab78b8416d93d1eb8e62ae51862a608d6
seoyeon0103 <angel6462@naver.com>
2025-02-05 15:42:31 +0900

    ♻️ smokingareaService 에서 getCenterLocationFromKakao 함수 사용 제거

--- src/main/java/com/ssmoker/smoker/domain/smokingArea/repository/SmokingAreaRepository.java
+++ src/main/java/com/ssmoker/smoker/domain/smokingArea/repository/SmokingAreaRepository.java
@@ -50,4 +50,18 @@ public interface SmokingAreaRepository extends JpaRepository<SmokingArea, Long> 
             @Param("search") String search,
             @Param("centerLat") Double centerLat,
             @Param("centerLng") Double centerLng);
+
+    //위도 경도 차 구하기
+    @Query(value = """
+    SELECT MAX(s.latitude) - MIN(s.latitude) as lat_diff
+    from smoking_area s 
+    where s.address LIKE CONCAT('%', :search, '%')""", nativeQuery = true)
+    Number findLatDiff(@Param("search") String search);
+
+    @Query(value = """
+    SELECT MAX(s.longitude) - MIN(s.longitude) as lng_diff
+    from smoking_area s 
+    where s.address LIKE CONCAT('%', :search, '%')""", nativeQuery = true)
+    Number findLngDiff(@Param("search") String search);
+
 }
