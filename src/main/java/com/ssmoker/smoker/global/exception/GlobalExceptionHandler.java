46995de808a0aedd31b4dcbabf28f64d6d32251d
heehunjung <heehunjung3658@gmail.com>
2025-03-08 00:29:41 +0900

    feat: NoResourceFoundException, HttpRequestMethodNotSupportedException 예외 핸들러 추가
    HttpRequestMethodNotSupportedException: 잘못된 http 메서드 요청이 오는 경우
    NoResourceFoundException: 데이터베이스에서 리소스를 찾지 못하는 경우 발생하는 예외

--- src/main/java/com/ssmoker/smoker/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ssmoker/smoker/global/exception/GlobalExceptionHandler.java
@@ -4,42 +4,62 @@ import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
+    // @Validated. @PathVariable, @RequestParam
     @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity constraintViolationException(ConstraintViolationException exception) {
+    public ResponseEntity constraintViolationException(final ConstraintViolationException exception) {
         log.error("message: ", exception);
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body("입력값이 유효하지 않습니다.");
     }
 
+    // @Validate. @RequestBody
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity methodArgumentNotValidException(MethodArgumentNotValidException exception) {
+    public ResponseEntity methodArgumentNotValidException(final MethodArgumentNotValidException exception) {
         log.error("message: ", exception);
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body("입력값이 유효하지 않습니다.");
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity handleHttpRequestMethodNotSupportedException(
+            final HttpRequestMethodNotSupportedException exception) {
+        log.warn("message: {}", exception.getMessage());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("잘못된 HTTP 메서드입니다.");
+    }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity noResourceFoundException(final NoResourceFoundException exception) {
+        log.warn("message: {}", exception.getMessage());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("리소스를 찾을 수 없습니다.");
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity exceptionHandler(Exception exception, WebRequest request) {
+    public ResponseEntity exceptionHandler(final Exception exception) {
         log.error("message: ", exception);
 
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body("서버 에러입니다. 관리자에게 문의해주세요.")
+                .body("서버 에러입니다. 관리자에게 문의해주세요.");
     }
 
     @ExceptionHandler(SmokerException.class)
-    public ResponseEntity smokerExceptionHandler(SmokerException exception) {
+    public ResponseEntity smokerExceptionHandler(final SmokerException exception) {
         log.warn("message: ", exception);
 
         return ResponseEntity.status(exception.getStatus())
