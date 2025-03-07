640ff5503a09f861159f0fe24092a25857d185c5
heehunjung <heehunjung3658@gmail.com>
2025-03-08 00:12:43 +0900

    refactor: webRequest 관련 삭제 및 코드 간소화
    아는 것만 쓰는게 낫다싶어 잘 알지못하는 클래스 사용을 제거했습니다.

--- src/main/java/com/ssmoker/smoker/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ssmoker/smoker/global/exception/GlobalExceptionHandler.java
@@ -1,106 +1,48 @@
 package com.ssmoker.smoker.global.exception;
 
-import com.ssmoker.smoker.global.apiPayload.ApiResponse;
-import com.ssmoker.smoker.global.exception.code.ErrorStatus;
 import jakarta.validation.ConstraintViolationException;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @Slf4j
-@RestControllerAdvice(annotations = {RestController.class})
+@RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler
-    public ResponseEntity<Object> validation(ConstraintViolationException exception, WebRequest request) {
-        String errorMessage = exception.getConstraintViolations().stream()
-                .map(constraintViolation -> constraintViolation.getMessage())
-                .findFirst()
-                .orElseThrow(() -> new RuntimeException("ConstraintViolationException 추출 도중 에러 발생"));
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity constraintViolationException(ConstraintViolationException exception) {
+        log.error("message: ", exception);
 
-        return handleExceptionInternalConstraint(exception, ErrorStatus.valueOf(errorMessage), HttpHeaders.EMPTY, request);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("입력값이 유효하지 않습니다.");
     }
 
-    @ExceptionHandler
-    public ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException exception, HttpHeaders headers,
-                                                               HttpStatusCode status, WebRequest request) {
-        Map<String, String> errors = new LinkedHashMap<>();
-        exception.getBindingResult().getFieldErrors().stream()
-                .forEach(fieldError -> {
-                    String fieldName = fieldError.getField();
-                    String errorMessage = Optional.ofNullable(fieldError.getDefaultMessage()).orElse("");
-                    errors.merge(fieldName, errorMessage,
-                            (existingErrorMessage, newErrorMessage) -> existingErrorMessage + ", " + newErrorMessage);
-                });
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity methodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        log.error("message: ", exception);
 
-        return handleExceptionInternalArgs(exception, HttpHeaders.EMPTY, ErrorStatus.valueOf("_BAD_REQUEST"), request, errors);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("입력값이 유효하지 않습니다.");
     }
 
-    @ExceptionHandler
-    public ResponseEntity<Object> exceptionHandler(Exception exception, WebRequest request) {
-        log.warn("message: ", exception);
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity exceptionHandler(Exception exception, WebRequest request) {
+        log.error("message: ", exception);
 
-        return handleExceptionInternalFalse(exception, ErrorStatus._INTERNAL_SERVER_ERROR, HttpHeaders.EMPTY,
-                ErrorStatus._INTERNAL_SERVER_ERROR.getHttpStatus(), request, exception.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("서버 에러입니다. 관리자에게 문의해주세요.")
     }
 
-    @ExceptionHandler(value = SmokerException.class)
+    @ExceptionHandler(SmokerException.class)
     public ResponseEntity smokerExceptionHandler(SmokerException exception) {
         log.warn("message: ", exception);
 
         return ResponseEntity.status(exception.getStatus())
                 .body(exception.getMessage());
-    }
-
-    private ResponseEntity<Object> handleExceptionInternalFalse(Exception e, ErrorStatus errorCommonStatus,
-                                                                HttpHeaders headers, HttpStatus status,
-                                                                WebRequest request, String errorPoint) {
-        ApiResponse<Object> body = ApiResponse.onFailure(errorCommonStatus.getCode(), errorCommonStatus.getMessage(),
-                errorPoint);
-        return super.handleExceptionInternal(
-                e,
-                body,
-                headers,
-                status,
-                request
-        );
-    }
-
-    private ResponseEntity<Object> handleExceptionInternalArgs(Exception e, HttpHeaders headers,
-                                                               ErrorStatus errorCommonStatus,
-                                                               WebRequest request, Map<String, String> errorArgs) {
-        ApiResponse<Object> body = ApiResponse.onFailure(errorCommonStatus.getCode(), errorCommonStatus.getMessage(),
-                errorArgs);
-        return super.handleExceptionInternal(
-                e,
-                body,
-                headers,
-                errorCommonStatus.getHttpStatus(),
-                request
-        );
-    }
-
-    private ResponseEntity<Object> handleExceptionInternalConstraint(Exception e, ErrorStatus errorCommonStatus,
-                                                                     HttpHeaders headers, WebRequest request) {
-        ApiResponse<Object> body = ApiResponse.onFailure(errorCommonStatus.getCode(), errorCommonStatus.getMessage(),
-                null);
-        return super.handleExceptionInternal(
-                e,
-                body,
-                headers,
-                errorCommonStatus.getHttpStatus(),
-                request
-        );
     }
 }
