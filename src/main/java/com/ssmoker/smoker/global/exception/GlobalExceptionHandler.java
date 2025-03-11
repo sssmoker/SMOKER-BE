7f8af2574a1d1c36ca76caa902454020ba8579a2
heehunjung <heehunjung3658@gmail.com>
2025-03-11 17:13:05 +0900

    chore: 오타 수정

--- src/main/java/com/ssmoker/smoker/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ssmoker/smoker/global/exception/GlobalExceptionHandler.java
@@ -10,7 +10,7 @@ import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-=import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @Slf4j
